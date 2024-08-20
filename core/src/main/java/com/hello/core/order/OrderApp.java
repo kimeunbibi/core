674d2175723dbca3dbd50853465776dd8519b12b
eb <tlafd@hanmail.net>
2024-08-20 20:47:33 +0900

    스프링 핵심 원리 - 기본편 최초생성

---
+++ core/src/main/java/com/hello/core/order/OrderApp.java
@@ -0,0 +1,25 @@
+package com.hello.core.order;
+
+import com.hello.core.AppConfig;
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import com.hello.core.member.MemberService;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("order.calculatePrice() = " + order.calculatePrice());
+
+    }
+}
